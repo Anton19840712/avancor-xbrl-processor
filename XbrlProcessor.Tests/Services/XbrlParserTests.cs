29a32bfb9b56c4ddc738e20e40bfa4c6d4af5636
Anton Gridushko <anton.gridushko@gmail.com>
2026-01-30 03:31:50 +0300

    test: добавить edge case тесты для XbrlParser
    1 do: Тесты на XbrlParseException: missing id, bad date, missing unit id
      so: покрыты все ветки defensive parsing из предыдущего коммита
      in: XbrlParserTests.cs, test-context-no-id.xml, test-context-bad-date.xml, test-unit-no-id.xml (new)
    
    2 do: Тесты на graceful handling: missing entity, missing period, empty value, empty report
      so: подтверждено что парсер не падает на неполных но валидных документах
      in: XbrlParserTests.cs, test-context-missing-entity.xml, test-context-missing-period.xml, test-fact-empty-value.xml, test-empty-report.xml (new)
    
    3 do: Тест на forever period
      so: покрыта ветка которая раньше тестировалась только через реальные отчёты
      in: XbrlParserTests.cs, test-context-forever.xml (new)

--- XbrlProcessor.Tests/Services/XbrlParserTests.cs
+++ XbrlProcessor.Tests/Services/XbrlParserTests.cs
@@ -1,6 +1,6 @@
-using System.Xml.Linq;
 using FluentAssertions;
 using XbrlProcessor.Configuration;
+using XbrlProcessor.Models.Entities;
 using XbrlProcessor.Services;
 
 namespace XbrlProcessor.Tests.Services;
@@ -151,7 +151,9 @@ public class XbrlParserTests
         fact.Id.Should().Be("TestFact");
         fact.ContextRef.Should().Be("C1");
         fact.UnitRef.Should().Be("RUB");
-        fact.Value.Should().Be("1000000.50");
+        fact.Value.Type.Should().Be(XbrlValueType.Numeric);
+        fact.Value.NumericValue.Should().Be(1000000.50m);
+        fact.Value.ToString().Should().Be("1000000.50");
         fact.Decimals.Should().Be(2);
         fact.Precision.Should().BeNull();
     }
@@ -229,4 +231,114 @@ public class XbrlParserTests
             }
         }
     }
+
+    #region Edge Case Tests
+
+    [Fact]
+    public void ParseXbrlFile_WithContextMissingId_ShouldThrowXbrlParseException()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-context-no-id.xml");
+
+        Action act = () => _parser.ParseXbrlFile(filePath);
+
+        act.Should().Throw<XbrlParseException>()
+            .WithMessage("*missing required 'id' attribute*");
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithBadDate_ShouldThrowXbrlParseException()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-context-bad-date.xml");
+
+        Action act = () => _parser.ParseXbrlFile(filePath);
+
+        act.Should().Throw<XbrlParseException>()
+            .WithMessage("*Invalid date value 'not-a-date'*");
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithUnitMissingId_ShouldThrowXbrlParseException()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-unit-no-id.xml");
+
+        Action act = () => _parser.ParseXbrlFile(filePath);
+
+        act.Should().Throw<XbrlParseException>()
+            .WithMessage("*Unit*missing required 'id' attribute*");
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithMissingEntity_ShouldParseWithNullEntityFields()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-context-missing-entity.xml");
+
+        var instance = _parser.ParseXbrlFile(filePath);
+
+        instance.Contexts.Should().HaveCount(1);
+        var context = instance.Contexts.First();
+        context.Id.Should().Be("C1");
+        context.EntityValue.Should().BeNull();
+        context.EntityScheme.Should().BeNull();
+        context.PeriodInstant.Should().Be(new DateTime(2019, 4, 30));
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithMissingPeriod_ShouldParseWithNullPeriodFields()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-context-missing-period.xml");
+
+        var instance = _parser.ParseXbrlFile(filePath);
+
+        instance.Contexts.Should().HaveCount(1);
+        var context = instance.Contexts.First();
+        context.Id.Should().Be("C1");
+        context.EntityValue.Should().Be("1234567890");
+        context.PeriodInstant.Should().BeNull();
+        context.PeriodStartDate.Should().BeNull();
+        context.PeriodEndDate.Should().BeNull();
+        context.PeriodForever.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithEmptyFactValue_ShouldParseAsEmptyString()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-fact-empty-value.xml");
+
+        var instance = _parser.ParseXbrlFile(filePath);
+
+        instance.Facts.Should().HaveCount(1);
+        var fact = instance.Facts.First();
+        fact.Value.Type.Should().Be(XbrlValueType.String);
+        fact.Value.ToString().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithEmptyReport_ShouldReturnEmptyInstance()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-empty-report.xml");
+
+        var instance = _parser.ParseXbrlFile(filePath);
+
+        instance.Should().NotBeNull();
+        instance.Contexts.Should().BeEmpty();
+        instance.Units.Should().BeEmpty();
+        instance.Facts.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ParseXbrlFile_WithForeverPeriod_ShouldParseForeverFlag()
+    {
+        var filePath = Path.Combine(_testDataPath, "test-context-forever.xml");
+
+        var instance = _parser.ParseXbrlFile(filePath);
+
+        instance.Contexts.Should().HaveCount(1);
+        var context = instance.Contexts.First();
+        context.PeriodForever.Should().BeTrue();
+        context.PeriodInstant.Should().BeNull();
+        context.PeriodStartDate.Should().BeNull();
+        context.PeriodEndDate.Should().BeNull();
+    }
+
+    #endregion
 }
